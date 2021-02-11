4c0e6f43ebb7e674147fc72701fe53bb679d49b1
Munuve30 <77729985+Munuve30@users.noreply.github.com>
2021-02-11 11:05:40 +0300

    Add files via upload

---
+++ Identity.py
@@ -0,0 +1,8 @@
+#identity operators include is and is not
+#is-returns true if both variables are the same object
+#is not-returns true if both variables are not the same object
+x=["apple","banana"]
+y=["apple", "banana"]
+z=x
+print(x is z)
+print(x is not y)
